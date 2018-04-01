74ba39f21247623d0e9ef3b2a48b1692146758d0
Daniel Watson <dwatson251@gmail.com>
2018-04-01 21:33:20 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+        <form class="" action="index.html" method="post">
+            <input type="text" name="username" placeholder="username" />
+            <input type="text" name="password" placeholder="password" />
+            <button>Login</button>
+        </form>
+    </body>
+</html>
