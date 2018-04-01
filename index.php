479c1aaf09545381c61b65455f89451262b21564
Daniel Watson <dwatson251@gmail.com>
2018-04-01 21:49:53 +0100

    Added basic login

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
+<?php
+
+    $loggedin = false;
+
+    $username = 'admin';
+    $password = 'admin123';
+
+    if(isset($_POST['username'] && $_POST['password'])) {
+        if($_POST['username'] === $username && $_POST['password'] === $password)) {
+            $loggedin = true;
+        }
+    }
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
     <head>
@@ -5,10 +21,16 @@
         <title></title>
     </head>
     <body>
-        <form class="" action="index.html" method="post">
-            <input type="text" name="username" placeholder="username" />
-            <input type="text" name="password" placeholder="password" />
-            <button>Login</button>
-        </form>
+        <?php if(!$loggedin): ?>
+            <form class="" action="index.html" method="post">
+                <input type="text" name="username" placeholder="username" />
+                <input type="text" name="password" placeholder="password" />
+                <button>Login</button>
+            </form>
+        <?php else; ?>
+
+            <h1>Welcome to your gym</h1>
+
+        <?php endif; ?>
     </body>
 </html>
