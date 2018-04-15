c473c2e58c83c8b30d01bd71edf4e20eb11d5ef6
Daniel Watson <dwatson251@gmail.com>
2018-04-15 19:05:03 +0100

    Changed the title of the welcome screen

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Gym Management
                 </div>
 
                 <div class="links">
